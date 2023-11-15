3fe86397c71b521a0a549267fedfad6811b77722
Kenny Sutherland <kennymax18@gmail.com>
2023-11-15 10:19:50 -0500

    add auth redirects

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
 
 export default function Home() {
+  const cookieStore = cookies();
+  const jwtToken = cookieStore.get("session_id");
+  if (jwtToken && jwtToken.value && process.env.JWT_SECRET) {
+    const decoded = jwt.verify(
+      jwtToken.value,
+      process.env.JWT_SECRET
+    ) as jwt.JwtPayload;
+    if (decoded.type == "patient-user") {
+      redirect("/patient");
+    }
+    redirect("/doctor");
+  }
   redirect("/login");
-
-  return (
-    <main className="">
-      <p>Loading...</p>
-    </main>
-  );
 }
